ab89fe33180610150a67ea05f787b5900b014d0e
Jasmine Javadi <jasmine.javadi@gmail.com>
2018-10-30 15:31:25 -0400

    ex6: 'proto' was used instead of 'prototype' (proto>prototype)

--- exercises/exercise6/js/Paddle.js
+++ exercises/exercise6/js/Paddle.js
@@ -12,7 +12,7 @@ function Paddle(x,y,w,h,speed,downKey,upKey) {
   this.y = y;
   //////////////// FIXED
   this.xv = xv;
-  //////////////// FIXED
+  //////////////// FIXED x2
   this.vy = vy;
   this.w = w;
   this.h = h;
@@ -26,7 +26,8 @@ function Paddle(x,y,w,h,speed,downKey,upKey) {
 //
 // Check if the up or down keys are pressed and update velocity
 // appropriately
-Paddle.proto.handleInput = function() {
+//////////////// FIXED
+Paddle.prototype.handleInput = function() {
   if (keyDown(upKey)) {
     this.vy = -this.speed;
   }
