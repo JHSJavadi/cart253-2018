ba5c10ddb55c1f6fd68026db06f41eff60188137
Jasmine Javadi <jasmine.javadi@gmail.com>
2018-10-30 16:16:08 -0400

    ex6: paddles were going to the bottom of the screen right away with no way to move them

--- exercises/exercise6/js/Paddle.js
+++ exercises/exercise6/js/Paddle.js
@@ -30,11 +30,15 @@ function Paddle(x,y,vx,vy,w,h,speed,downKey,upKey) {
 Paddle.prototype.handleInput = function() {
   //////////////// FIXEDx2
   if (keyIsDown(this.upKey)) {
-    this.vy = -this.speed;
+    this.vy = this.speed;
   }
   //////////////// FIXEDx2
   else if (keyIsDown(this.downKey)) {
     this.vy = -this.speed;
+  }
+  //////////////// FIXED
+  else {
+    this.vy = 0;
   }
 }
 
